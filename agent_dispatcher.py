7a949d4d5f3ca4509c84491cf7011becf081ab3a
mateicaragea31 <matei.caragea31@gmail.com>
2025-11-16 11:07:30 +0200

    Finished Agent Dispatcher

--- agent_dispatcher.py
+++ agent_dispatcher.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field
 from datetime import date
 from typing import List
@@ -9,6 +10,9 @@ from db_connector import db, firestore
 
 # --- 1. Definirea Modelului de Date (Contractul API) ---
 class DeliveryRequest(BaseModel):
+    client_name: str = Field(...,
+                             description = "Numele clientului",
+                             examples = ["Popescu Ion"])
     awb: str = Field(..., 
                      description="Numărul AWB al coletului.", 
                      examples=["AWB123456"])
@@ -21,6 +25,31 @@ class DeliveryRequest(BaseModel):
     available_delivery_dates: List[date] = Field(...,
                                                   description="O listă cu datele în care curierul POATE livra.",
                                                   examples=[["2025-11-18", "2025-11-19"]])
+    delivery_method: str = Field(...,
+                                 description = "Delivery option chosed by client.",
+                                 examples = ["locker", "home"])
+    country: str = Field(...,
+                         description = "Tara locuintei clientului.",
+                         examples = ["Romania", "RO"])
+    city: str = Field(...,
+                      description="Orașul de reședință.",
+                      examples=["București"])
+    county: str = Field(...,
+                      description="Județul sau sectorul.",
+                      examples=["Sector 1"])
+    street_name: str = Field(...,
+                             description="Numele străzii.",
+                             examples=["Calea Victoriei"])
+    street_number: str = Field(...,
+                               description="Numărul străzii.",
+                               examples=["100"])
+    postal_code: str = Field(...,
+                                description="Codul poștal (opțional).",
+                                examples=["010071"])
+    details: str = Field(...,
+                            description="Detalii suplimentare (bloc, scară, ap, interfon).",
+                            examples=["Bloc D, Scara 2, Ap. 30, Interfon 30C"])
+
 
 # --- 2. Inițializarea Aplicației FastAPI ---
 app = FastAPI(
@@ -42,7 +71,7 @@ async def receive_delivery_notification(request: DeliveryRequest):
     Acesta este endpoint-ul pe care îl apelează FanCourier.
     El primește JSON-ul și îl salvează în Firestore.
     """
-    logger.info(f"Primit notificare pentru AWB: {request.awb} de la {request.source_courier}")
+    logger.info(f"Primit notificare pentru AWB: {request.awb} de la {request.source_courier}. Nume client: {request.client_name}.")
 
     if db is None:
         logger.error("Eroare critică: Conexiunea la DB nu este activă.")
@@ -52,7 +81,7 @@ async def receive_delivery_notification(request: DeliveryRequest):
         # 'request.model_dump(mode="json")' convertește JSON-ul primit 
         # (care e acum un obiect Pydantic) într-un dicționar
         # care poate fi salvat în Firebase (convertește 'date' în 'str').
-        data_to_save = request.model_dump(mode="json")
+        data_to_save = jsonable_encoder(request)
         
         # Adăugăm câmpuri extra pentru statusul intern
         data_to_save['status'] = 'received' # Statusul inițial
