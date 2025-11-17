c5a19104e2f52b696d1a312b657921d588b36fa8
mateicaragea31 <matei.caragea31@gmail.com>
2025-11-17 19:49:55 +0200

    Finished Project

--- playground.py
+++ playground.py
@@ -48,6 +48,5 @@ def whatsapp_reply():
     return str(incoming_message)
 
 
-# Pornește serverul pe portul 5000
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5001, debug=True, use_reloader = False)
